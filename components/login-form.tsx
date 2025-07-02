afd911cd56742f58aab267f8df4d86dabc3e810a
suhail <suhail609@gmail.com>
2025-07-02 16:31:41 +0400

    refactor(login): use zod

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -18,23 +18,46 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { UserRole, UserSignin } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import type React from "react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const SigninSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string(),
+  role: z.enum(Object.values(UserRole) as [UserRole, ...UserRole[]], {
+    required_error: "Role is required",
+    invalid_type_error: "Invalid role",
+  }),
+});
+
+type SigninFormValues = z.infer<typeof SigninSchema>;
 
 interface LoginFormProps {
   onLogin: (user: UserSignin) => void;
 }
 
 export function LoginForm({ onLogin }: LoginFormProps) {
+  const {
+    watch,
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isSubmitting },
+  } = useForm<SigninFormValues>({
+    resolver: zodResolver(SigninSchema),
+  });
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [selectedRole, setSelectedRole] = useState<UserRole>(UserRole.EMPLOYEE);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const onSubmit = (data: SigninFormValues) => {
+    // e.preventDefault();
 
-    onLogin({ email: email, password: password, role: selectedRole });
+    onLogin({ email: data.email, password: data.password, role: data.role });
 
     // if (user) {
     //   onLogin(user)
@@ -51,17 +74,22 @@ export function LoginForm({ onLogin }: LoginFormProps) {
           <CardDescription>Sign in to access your timesheet</CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit} className="space-y-4">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Email</Label>
               <Input
                 id="email"
                 type="email"
                 placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                // value={email}
+                {...register("email")}
+                // onChange={(e) => setEmail(e.target.value)}
                 required
+                className={errors.email ? "border-red-500" : ""}
               />
+              {errors.email && (
+                <p className="text-sm text-red-500">{errors.email.message}</p>
+              )}
             </div>
             <div className="space-y-2">
               <Label htmlFor="password">Password</Label>
@@ -69,19 +97,27 @@ export function LoginForm({ onLogin }: LoginFormProps) {
                 id="password"
                 type="password"
                 placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                // value={password}
+                // onChange={(e) => setPassword(e.target.value)}
                 required
+                {...register("password")}
+                className={errors.password ? "border-red-500" : ""}
               />
+              {errors.password && (
+                <p className="text-sm text-red-500">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
 
             <div className="space-y-2">
               <Label htmlFor="role">Role</Label>
               <Select
-                value={selectedRole}
-                onValueChange={(value: UserRole) => setSelectedRole(value)}
+                value={watch("role")}
+                onValueChange={(value: UserRole) => setValue("role", value)}
+                // onValueChange={(value: UserRole) => setSelectedRole(value)}
               >
-                <SelectTrigger>
+                <SelectTrigger className={errors.role ? "border-red-500" : ""}>
                   <SelectValue placeholder="Select your role" />
                 </SelectTrigger>
                 <SelectContent>
@@ -92,6 +128,9 @@ export function LoginForm({ onLogin }: LoginFormProps) {
                   ))}
                 </SelectContent>
               </Select>
+              {errors.role && (
+                <p className="text-sm text-red-500">{errors.role.message}</p>
+              )}
             </div>
 
             <Button type="submit" className="w-full">
